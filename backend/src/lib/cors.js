a1a88492881e263b7decf68e756e99e6f3a836e8
pipipat <Natpat19761976@gmail.com>
2026-09-15 13:45:44 +0700

    edit access

--- backend/src/lib/cors.js
+++ backend/src/lib/cors.js
@@ -2,7 +2,7 @@ let corsHeaders = {
   "Access-Control-Allow-Credentials": "true",
   "Access-Control-Allow-Origin": "https://next-js-4-password-bgys.vercel.app",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, x-user-id, x-user-username, x-user-email",
   "Access-Control-Max-Age": "86400",
 };
 
